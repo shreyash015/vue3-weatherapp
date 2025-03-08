a837c901dabeef7bf41b7c744c66c78a4551ee6b
shreyash015 <shreyash015@gmail.com>
2025-03-08 01:16:19 -0600

    created a search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+import searchbarComponent from "./components/searchbarComponent.vue";
+</script>
 
 <template>
   <div>
     <h1>The Weather App</h1>
+    <searchbarComponent />
   </div>
 </template>
 
